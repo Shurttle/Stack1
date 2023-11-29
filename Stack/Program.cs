a14b0ec145b6b21a6d21996b336f5a5c8c5cb4f1
FESQO <98529236+FESQO@users.noreply.github.com>
2023-11-29 10:05:38 +1000

    Добавьте файлы проекта.

---
+++ Stack/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+
+public class StackItem<T>
+{
+    public StackItem<T> Previos { get; set; }
+    public T Value { get; set; }
+}
+public class MyStack<T>
+{
+    private StackItem<T> bells;
+    public void Push(T value)
+    {
+        var newItem = new StackItem<T> { Value = value, Previos = bells };
+        bells = newItem;
+    }
+    public T Pop()
+    { 
+        if (bells == null)
+        {
+            return default(T);
+        }
+        var value = bells.Value;
+        bells = bells.Previos;
+        return value;
+    }
+    public T Peek()
+    {
+        if (bells == null) 
+        { 
+        return default (T);
+        }
+        return bells.Value;
+    }
+    public int Count
+    {
+        //aaaaaaaaaaaaaaaaaaaaaaaaaaa
+        get
+        {
+            int count = 0;
+            var current = bells;
+            while (current != null) 
+            { 
+            count++;
+                current = current.Previos;
+            }
+            return count;
+        }
+    }
+    public void Clear() 
+    {
+        bells = null;
+    }
+}
