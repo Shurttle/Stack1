08a2dc891131b33c7435597ecf7ef840986b4a46
SnegokPNG <dmitrij_borisov_06@mail.ru>
2023-12-04 14:59:04 +0300

    правки 2

---
+++ Stack/Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Queue<T>
+{
+    private class QueueItem
+    {
+        public T Value { get; }
+        public QueueItem Next { get; set; }
+
+        public QueueItem(T value)
+        {
+            Value = value;
+        }
+    }
+
+    private QueueItem _head;
+    private QueueItem _tail;
+
+    public void Enqueue(T value)
+    {
+        var newItem = new QueueItem(value);
+        if (_head == null)
+        {
+            _head = newItem;
+            _tail = newItem;
+        }
+        else
+        {
+            _tail.Next = newItem;
+            _tail = newItem;
+        }
+    }
+
+    public T Dequeue()
+    {
+        if (_head == null)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        var value = _head.Value;
+        _head = _head.Next;
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (_head == null)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        return _head.Value;
+    }
+
+    public bool IsEmpty()
+    {
+        return _head == null;
+    }
+}
---
+++ Stack/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Mark { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        Stack stack1 = new Stack();
+        Stack stack2 = new Stack();
+        for (int i = 0; i < 5; i++)
+        {
+            Student student = new Student();
+            student.FirstName = "Anton" + (i + 1);
+            student.LastName = "Churka" + (i + 1);
+            stack1.Push(student);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Student student = (Student)stack1.Pop();
+            Console.WriteLine("Введите оценку " + student.FirstName + student.LastName);
+            int mark = Convert.ToInt32(Console.ReadLine());
+            student.Mark = mark;
+            stack2.Push(student);
+        }
+    }
+}
